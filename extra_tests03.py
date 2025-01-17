6e4514879d72b153ef61627d6b0acd3eb504e01c
fridayshoes <one_amp@hotmail.com>
2025-01-17 17:46:32 +0000

    capitalising the first letter of each word in a string

---
+++ extra_tests03.py
@@ -0,0 +1,44 @@
+
+
+# String Challenge
+
+# Have the function StringChallenge (str) take the str parameter being passed and
+# capitalize the first letter of each word. Words will be separated by only one space.
+
+# Examples
+
+# Input: "hello world"
+# Output: Hello World
+
+# Input: "i ran there"
+# Output: I Ran There
+
+
+example1 = "hello world"
+example2 = "i ran there"
+
+
+
+
+
+def StringChallenge(strParam):
+
+# code goes here
+
+  return strParam.title()
+
+
+
+print("Example 1")
+print(StringChallenge(example1))
+
+print("Example 2")
+print(StringChallenge(example2))
+
+
+
+
+
+
+# keep this function call here
+# print StringChallenge(raw_input())
